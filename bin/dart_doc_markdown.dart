b1f04cd57829db1a89dc859f69ddadb00db65ccb
Scott Hatfield <contact@hatfieldscott.com>
2025-01-24 06:32:05 -0600

    Created Dart CLI from template :rocket:

---
+++ bin/dart_doc_markdown.dart
@@ -0,0 +1,5 @@
+import 'package:dart_doc_markdown/dart_doc_markdown.dart' as dart_doc_markdown;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_doc_markdown.calculate()}!');
+}
---
+++ test/dart_doc_markdown_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_doc_markdown/dart_doc_markdown.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
